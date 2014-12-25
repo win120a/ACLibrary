911650355db564ae62d16cff3ff67f28f9a75aa2
Andy Cheung <micro_zx@msn.cn>
2014-12-25 23:01:32 +0800

    Update

---
+++ ACCryptoAPI/Utils/CryptoEntry.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ACLibrary.Crypto.Utils
+{
+    public class CryptoEntry
+    {
+        public static string Encrypt(ICryptoProvider icp, string otext, string psw)
+        {
+            return icp.EncryptString(otext, psw);
+        }
+
+        public static string Decrypt(ICryptoProvider icp, string otext, string psw)
+        {
+            return icp.DecryptString(otext, psw);
+        }
+    }
+}
