06ef41e9a556d05d38004df4da030b633a59091c
Andy Cheung <micro_zx@msn.cn>
2015-07-09 20:55:39 +0800

    Update

---
+++ ACCryptoAPI/RandomKeyMixCrypt/Mid.cs
@@ -0,0 +1,96 @@
+﻿/*
+   Copyright (C) 2011-2015 AC Inc. (Andy Cheung)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+/*
+ * Based on MVA's 20 C# Question Explained.
+ * Copyright (C) Microsoft Corporation
+ */
+
+using ACLibrary.Crypto.CryptoProviders;
+using ACLibrary.TypeConvert;
+using System;
+using System.Collections.Generic;
+
+namespace ACLibrary.Crypto.RandomKeyMixCrypt
+{
+    public class Mid : IRandomKeyMixCryptBase
+    {
+        /// <summary>
+        /// The Encryption method.
+        /// </summary>
+        /// <param name="plainText">The string to encrypt.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>The encrypted string.</returns>
+        public ReturnStruct EncryptString(String plainText, String partPassword)
+        {
+            List<int> ril = new List<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                ril.Add(r.Next(0, 10));
+            }
+
+            List<string> sl = NumberConverter.IntCollectionToStringList(ril);
+
+            // 3DES
+            DESProvider des = new DESProvider();
+            string des1 = des.EncryptString(plainText, partPassword + sl[0]);
+            string des2 = des.EncryptString(des1, partPassword + sl[1]);
+            string des3 = des.EncryptString(des2, partPassword + sl[2]);
+
+            // 3AES
+            AESProvider aes = new AESProvider();
+            string aes1 = aes.EncryptString(des3, partPassword + sl[3]);
+            string aes2 = aes.EncryptString(aes1, partPassword + sl[4]);
+            string aes3 = aes.EncryptString(aes2, partPassword + sl[5]);
+
+            ReturnStruct rs = new ReturnStruct();
+
+            rs.Result = aes3;
+            rs.RandomKeys = ril.ToArray();
+
+            return rs;
+
+            // Use Casts: aes(aes(aes(des(des(des($content))))));
+        }
+
+        /// <summary>
+        /// The Decryption method.
+        /// </summary>
+        /// <param name="Source">The string to decrypt.</param>
+        /// <param name="password">The password.</param>
+        /// <returns>The decrypted string.</returns>
+        public String DecryptString(String Source, String password, int[] rKeys)
+        {
+            // string plain = testEncrypt.DecryptString(encText, password);
+
+            // 3AES
+            AESProvider aes = new AESProvider();
+            string aes1 = aes.DecryptString(Source, password + rKeys[5]);
+            string aes2 = aes.DecryptString(aes1, password + rKeys[4]);
+            string aes3 = aes.DecryptString(aes2, password + rKeys[3]);
+
+            // 3DES
+            DESProvider des = new DESProvider();
+            string des1 = des.DecryptString(aes3, password + rKeys[2]);
+            string des2 = des.DecryptString(des1, password + rKeys[1]);
+            string des3 = des.DecryptString(des2, password + rKeys[0]);
+
+            return des3;
+        }
+    }
+}
---
+++ TestRandomKeyMixCrypt/Program.cs
@@ -0,0 +1,47 @@
+﻿using ACLibrary.Crypto.RandomKeyMixCrypt;
+using System;
+using System.Text;
+
+namespace TestRandomKeyMixCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mid engine = new Mid();
+
+            ReturnStruct r = engine.EncryptString("test", "abc123");
+
+            Console.WriteLine(r.Result);
+            Console.WriteLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("[");
+
+            int it = 0;
+
+            foreach (int ti in r.RandomKeys)
+            {
+                sb.Append(ti);
+                if (it != (r.RandomKeys.Length) - 1)
+                {
+                    sb.Append(",");
+                }
+                it++;
+            }
+
+            sb.Append("]");
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(engine.DecryptString(r.Result, "abc123", r.RandomKeys));
+
+            Console.ReadLine();
+        }
+    }
+}
